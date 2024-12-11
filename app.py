ab64389d43461b2dd222cc43c98be1db69411090
ghassenSW <155910115+ghassenSW@users.noreply.github.com>
2024-12-11 12:53:00 +0100

    'commit'

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from flask_cors import CORS
 import re
 import os 
 from pymongo import MongoClient
@@ -8,7 +9,7 @@ from injury_updates import get_injury_updates_text
 
 app = Flask(__name__)
 app.secret_key = 'f3082ef12d47bf71416425c7eef8d573'
-
+CORS(app)
 # from dotenv import load_dotenv
 # load_dotenv()
 # MONGODB_URI=os.getenv('MONGODB_URI')
